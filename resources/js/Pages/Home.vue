c3415f24dcdee6b78edf22ffcb21e7378e7e1a91
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-08 11:10:10 +0000

    loop on tecknologies

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Hello, M.{{name}}</h1>
+
+    <p>list of tecknologies that should master this year :</p>
+
+    <ul>
+        <li v-for="tecknology of tecknologies" v-text="tecknology"></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        tecknologies: Array,
+    },
+};
+</script>
