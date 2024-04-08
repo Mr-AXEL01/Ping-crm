ff2d15f5878e6d7df0cf135b76df8556698ca6a2
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-08 11:18:56 +0000

    set routes for settings and users

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Home', [
-        'name'=> 'ABD_ELHAQ AZROUR',
-        'tecknologies' => [
-          'Laravel_11', 'Vue3', 'ReactJs' , 'Docker'
-        ],
-    ]);
+    return Inertia::render('Home');
+});
+
+Route::get('/users', function () {
+    return Inertia::render('Users');
+});
+
+Route::get('/settings', function () {
+    return Inertia::render('Settings');
 });
