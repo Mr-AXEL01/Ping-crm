0b45e1f4174f7f1e042d18f53f73f3db63b4f877
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-12 00:34:59 +0000

    create a pagination

---
+++ resources/js/Shared/Pagination.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <component
+            :is="link.url ? 'Link' : 'span'"
+            v-for="link in links"
+            :href="link.url"
+            v-html="link.label"
+            class="inline-block px-3 py-1 rounded-md bg-blue-100  mr-2"
+            :class="{'bg-gray-500': ! link.url, 'font-bold bg-blue-400' :link.active}"
+        />
+    </div>
+</template>
+<script>
+import {Link} from "@inertiajs/vue3";
+
+export default {
+    components: { Link },
+    props: {
+        links: Array
+    }
+};
+</script>
