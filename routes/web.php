a977d3b07872ce587c090e51eb0d8575fc3a3bac
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-04-19 20:28:32 +0100

    set the auth middlware and set route point  for login

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,37 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Models\User;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Home');
+Route::group(["middleware" => "auth"], function () {
+    Route::get("/", function (){
+     return Inertia::render("Home");
+    })->name("home");
 });
 
-Route::get('/users', function () {
+Route::get("/login", [LoginController::class, 'create'])->name("login");
+
+
+Route::get('/users/Index', function () {
     return Inertia::render('Users', [
-        'users' => User::paginate(10)->through(fn($user) => [
-            'id'   => $user->id,
-            'name' => $user->name,
-        ])
+        'users' => User::query()
+            ->when(Request::input('search'), function ($query, $search) {
+                $query->where('name', 'like', "%{$search}%");
+            })
+            ->paginate(10)
+            ->withQueryString()
+            ->through(fn($user) => [
+                'id'   => $user->id,
+                'name' => $user->name,
+            ]),
+        'filters' => Request::only(['search'])
     ]);
 });
 
-Route::get('/settings', function () {
-    return Inertia::render('Settings');
+
+Route::get('/users/create', function () {
+    return Inertia::render('Users/create');
 });
